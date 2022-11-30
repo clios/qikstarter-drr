61f1e6b215c57cc2db91df8689bafecc1b8b6d7c
Cliemtor Fabros <cliemtor08@gmail.com>
2022-11-30 09:58:38 +0800

    Add incident dashboard

--- src/fragments/IncidentDashboard/VictimAreaChart.jsx
+++ src/fragments/IncidentDashboard/VictimAreaChart.jsx
@@ -4,70 +4,88 @@ import { VictoryAxis, VictoryBar, VictoryChart, VictoryLabel, VictoryLine } from
 
 import React from 'react'
 
-function VictimAreaChart() {
+function VictimAreaChart({ data }) {
+  const date = new Date()
+  const colList = [
+    new Date(date.setMonth(date.getMonth())).toLocaleDateString('en-us', { month: 'short', day: '2-digit' }),
+    new Date(date.setMonth(date.getMonth() - 1)).toLocaleDateString('en-us', { month: 'short', day: '2-digit' }),
+    new Date(date.setMonth(date.getMonth() - 1)).toLocaleDateString('en-us', { month: 'short', day: '2-digit' }),
+    new Date(date.setMonth(date.getMonth() - 1)).toLocaleDateString('en-us', { month: 'short', day: '2-digit' }),
+    new Date(date.setMonth(date.getMonth() - 1)).toLocaleDateString('en-us', { month: 'short', day: '2-digit' }),
+    new Date(date.setMonth(date.getMonth() - 1)).toLocaleDateString('en-us', { month: 'short', day: '2-digit' }),
+    new Date(date.setMonth(date.getMonth() - 1)).toLocaleDateString('en-us', { month: 'short', day: '2-digit' }),
+    new Date(date.setMonth(date.getMonth() - 1)).toLocaleDateString('en-us', { month: 'short', day: '2-digit' }),
+    new Date(date.setMonth(date.getMonth() - 1)).toLocaleDateString('en-us', { month: 'short', day: '2-digit' }),
+    new Date(date.setMonth(date.getMonth() - 1)).toLocaleDateString('en-us', { month: 'short', day: '2-digit' }),
+    new Date(date.setMonth(date.getMonth() - 1)).toLocaleDateString('en-us', { month: 'short', day: '2-digit' }),
+    new Date(date.setMonth(date.getMonth() - 1)).toLocaleDateString('en-us', { month: 'short', day: '2-digit' })
+  ]
+
   return (
     <div>
-      <VictoryChart domainPadding={{ x: [30, 30], y: [0, 24] }} padding={{ left: 16, right: 16, bottom: 32, top: 32 }} height={170}>
-        <VictoryBar
-          labelComponent={<VictoryLabel dy={-5} />}
-          labels={({ datum }) => `${datum.y.toLocaleString()}`}
-          style={{ data: { fill: '#20A8DF', fillOpacity: 0.5, width: 24 }, labels: { fill: '#DF9C20', fontSize: 10 } }}
-          data={[
-            { x: '10-2022', y: 20 },
-            { x: '11-2022', y: 15 },
-            { x: '12-2022', y: 5 },
-            { x: '01-2022', y: 16 },
-            { x: '02-2022', y: 29 },
-            { x: '03-2022', y: 11 },
-            { x: '04-2022', y: 18 },
-            { x: '05-2022', y: 7 },
-            { x: '06-2022', y: 6 },
-            { x: '07-2022', y: 9 },
-            { x: '08-2022', y: 15 },
-            { x: '09-2022', y: 0 }
-          ]}
-        />
-        <VictoryLine
-          interpolation="step"
-          style={{ data: { stroke: '#DF9C20', strokeWidth: 1 } }}
-          data={[
-            { x: '10-2022', y: 20 },
-            { x: '11-2022', y: 15 },
-            { x: '12-2022', y: 5 },
-            { x: '01-2022', y: 16 },
-            { x: '02-2022', y: 29 },
-            { x: '03-2022', y: 11 },
-            { x: '04-2022', y: 18 },
-            { x: '05-2022', y: 7 },
-            { x: '06-2022', y: 6 },
-            { x: '07-2022', y: 9 },
-            { x: '08-2022', y: 15 },
-            { x: '09-2022', y: 0 }
-          ]}
-        />
-        <VictoryAxis
-          label="Victims in the last year"
-          style={{
-            axisLabel: { fill: '#DF9C20', fontSize: 9, padding: -130 },
-            tickLabels: { opacity: 0 }
-          }}
-        />
-        <VictoryAxis
-          label="151 Victims"
-          style={{
-            axisLabel: { fill: '#FFFFFF', fontSize: 9, padding: -120 },
-            tickLabels: { opacity: 0 }
-          }}
-        />
-        <VictoryAxis
-          style={{
-            axis: { stroke: 'white' },
-            tickLabels: { padding: 2, fontSize: 7, fontWeight: 100, fill: '#FFFFFF' },
-            grid: { stroke: '#73858C', strokeDasharray: '7' }
-          }}
-        />
-        <VictoryAxis dependentAxis style={{ tickLabels: { opacity: 0 }, grid: { stroke: '#73858C', strokeDasharray: '7' } }} />
-      </VictoryChart>
+      {data && (
+        <VictoryChart domainPadding={{ x: [30, 30], y: [0, 24] }} padding={{ left: 16, right: 16, bottom: 32, top: 32 }} height={170}>
+          <VictoryBar
+            labelComponent={<VictoryLabel dy={-5} />}
+            labels={({ datum }) => `${datum.y.toLocaleString()}`}
+            style={{ data: { fill: '#20A8DF', fillOpacity: 0.5, width: 24 }, labels: { fill: '#DF9C20', fontSize: 10 } }}
+            data={[
+              { x: colList[11], y: data[11] },
+              { x: colList[10], y: data[10] },
+              { x: colList[9], y: data[9] },
+              { x: colList[8], y: data[8] },
+              { x: colList[7], y: data[7] },
+              { x: colList[6], y: data[6] },
+              { x: colList[5], y: data[5] },
+              { x: colList[4], y: data[4] },
+              { x: colList[3], y: data[3] },
+              { x: colList[2], y: data[2] },
+              { x: colList[1], y: data[1] },
+              { x: colList[0], y: data[0] }
+            ]}
+          />
+          <VictoryLine
+            interpolation="step"
+            style={{ data: { stroke: '#DF9C20', strokeWidth: 1 } }}
+            data={[
+              { x: colList[11], y: data[11] },
+              { x: colList[10], y: data[10] },
+              { x: colList[9], y: data[9] },
+              { x: colList[8], y: data[8] },
+              { x: colList[7], y: data[7] },
+              { x: colList[6], y: data[6] },
+              { x: colList[5], y: data[5] },
+              { x: colList[4], y: data[4] },
+              { x: colList[3], y: data[3] },
+              { x: colList[2], y: data[2] },
+              { x: colList[1], y: data[1] },
+              { x: colList[0], y: data[0] }
+            ]}
+          />
+          <VictoryAxis
+            label="Victims in the last year"
+            style={{
+              axisLabel: { fill: '#DF9C20', fontSize: 9, padding: -130 },
+              tickLabels: { opacity: 0 }
+            }}
+          />
+          <VictoryAxis
+            label={`${data?.reduce((a, b) => a + b, 0)} Victims`}
+            style={{
+              axisLabel: { fill: '#FFFFFF', fontSize: 9, padding: -120 },
+              tickLabels: { opacity: 0 }
+            }}
+          />
+          <VictoryAxis
+            style={{
+              axis: { stroke: 'white' },
+              tickLabels: { padding: 2, fontSize: 7, fontWeight: 100, fill: '#FFFFFF' },
+              grid: { stroke: '#73858C', strokeDasharray: '7' }
+            }}
+          />
+          <VictoryAxis dependentAxis style={{ tickLabels: { opacity: 0 }, grid: { stroke: '#73858C', strokeDasharray: '7' } }} />
+        </VictoryChart>
+      )}
     </div>
   )
 }
--- src/pages/IncidentDashboard.jsx
+++ src/pages/IncidentDashboard.jsx
@@ -1,29 +1,74 @@
 import './IncidentDashboard.css'
 
+import AccountContext from '../contexts/AccountContext'
 import FadeAnimation from '../components/FadeAnimation'
 import IncidentAreaChart from '../fragments/IncidentDashboard/IncidentAreaChart'
 import IncidentBarChart from '../fragments/IncidentDashboard/IncidentBarChart'
+import Loader from '../components/Loader'
 import PageContent from '../components/PageContent'
 import React from 'react'
 import VictimAreaChart from '../fragments/IncidentDashboard/VictimAreaChart'
 import VictimBarChart from '../fragments/IncidentDashboard/VictimBarChart'
+import getDashboard from '../api/getDashboard'
 
 function IncidentDashboard() {
+  const IncidentsPerType = getDashboard('/incidents/perType')
+  const IncidentsPerMunicipal = getDashboard('/incidents/perMunicipal')
+  const IncidentsPerMonth = getDashboard('/incidents/perMonth')
+  const VictimsPerStatus = getDashboard('/victims/perStatus')
+  const VictimsPerMunicipal = getDashboard('/victims/perMunicipal')
+  const VictimsPerMonth = getDashboard('/victims/perMonth')
+
+  // INFORMATION STATE
+  const Account = React.useContext(AccountContext)
+  const [status, setStatus] = React.useState('loading')
+
+  React.useEffect(() => {
+    if (
+      IncidentsPerType.loading ||
+      IncidentsPerMunicipal.loading ||
+      IncidentsPerMonth.loading ||
+      VictimsPerStatus.loading ||
+      VictimsPerMunicipal.loading ||
+      VictimsPerMonth.loading
+    ) {
+      setStatus('loading')
+    }
+
+    if (IncidentsPerType.error) setStatus('error')
+
+    if (
+      IncidentsPerType.data &&
+      IncidentsPerMunicipal.data &&
+      IncidentsPerMonth.data &&
+      VictimsPerStatus.data &&
+      VictimsPerMunicipal.data &&
+      VictimsPerMonth.data
+    ) {
+      setStatus('success')
+    }
+
+    return () => setStatus('loading')
+  }, [IncidentsPerType, IncidentsPerMunicipal, IncidentsPerMonth, VictimsPerStatus, VictimsPerMunicipal, VictimsPerMonth])
+
   return (
-    <PageContent>
-      <FadeAnimation>
-        <div className="pid-row-one">
-          <IncidentBarChart />
-          <VictimBarChart />
-        </div>
-        <div>
-          <IncidentAreaChart />
-        </div>
-        <div>
-          <VictimAreaChart />
-        </div>
-      </FadeAnimation>
-    </PageContent>
+    <React.Fragment>
+      {status === 'loading' && <Loader />}
+      <PageContent>
+        <FadeAnimation>
+          <div className="pid-row-one">
+            <IncidentBarChart data={IncidentsPerType.data} />
+            <VictimBarChart data={VictimsPerStatus.data} />
+          </div>
+          <div>
+            <IncidentAreaChart data={IncidentsPerMonth.data} />
+          </div>
+          <div>
+            <VictimAreaChart data={VictimsPerMonth.data} />
+          </div>
+        </FadeAnimation>
+      </PageContent>
+    </React.Fragment>
   )
 }
 
